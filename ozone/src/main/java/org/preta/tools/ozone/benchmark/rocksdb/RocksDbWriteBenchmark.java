d171d1fc6c4a874a39b5966a5ec3e907c0595989
Nanda kumar <nandakumar131@gmail.com>
2020-02-20 00:45:51 +0530

    Flush option support in RocksDB benchmark

--- ozone/src/main/java/org/preta/tools/ozone/benchmark/rocksdb/RocksDbWriteBenchmark.java
+++ ozone/src/main/java/org/preta/tools/ozone/benchmark/rocksdb/RocksDbWriteBenchmark.java
@@ -27,11 +27,14 @@ import org.rocksdb.DBOptions;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 import org.rocksdb.Statistics;
+import org.rocksdb.WriteBatch;
+import org.rocksdb.WriteOptions;
 import picocli.CommandLine;
 
 import java.math.BigDecimal;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -40,7 +43,9 @@ import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static org.rocksdb.TickerType.COMPACT_READ_BYTES;
@@ -72,34 +77,53 @@ public class RocksDbWriteBenchmark implements Runnable {
       description = "Key Prefix.")
   private String keyNamePrefix;
 
-  @CommandLine.Option(names = {"-b", "--numBlocks"},
+  @CommandLine.Option(names = {"-n", "--numBlocks"},
       description = "Key Prefix.")
   private int numBlocks;
 
-  private static final String OPEN_KEY_TABLE = "OpenKeyTable";
+  @CommandLine.Option(names = {"-b", "--rocksDbBatchSize"},
+      description = "Key Prefix.")
+  private int rocksDbBatchSize;
+
+  @CommandLine.Option(names = {"-f", "--flush"},
+      description = "Flush RocksDB WAL.")
+  private boolean flush;
+
+  @CommandLine.Option(names = {"-s", "--sync"},
+      description = "Sync RocksDB WAL, applicable only if flush is enabled.")
+  private boolean sync;
+
   private static final String KEY_TABLE = "KeyTable";
 
   private static AtomicLong keyCount = new AtomicLong(0);
 
   private RocksDB db;
+  private long startTimeInNs;
   private long endTimeInNs;
   private Map<String, ColumnFamilyHandle> handlers = new HashMap<>();
   private Statistics statistics;
+  private WriteBatch batch;
+  private WriteOptions writeOptions;
+  private AtomicInteger currentBatchSize;
 
-  private Map<String, byte[]> openKeys = new HashMap<>();
-
-  private boolean useInMemoryOpenKeyStore;
+  private int deltaFlushCount = 0;
+  private long deltaFlushTime = 0;
 
   public RocksDbWriteBenchmark() {
     this.path = "/tmp/test.db";
     this.keyNamePrefix = "/instagram/images/";
     this.writerThreads = 10;
     this.numBlocks = 1;
+    this.rocksDbBatchSize = 25;
+    this.flush = false;
+    this.sync = false;
+    this.currentBatchSize = new AtomicInteger(0);
   }
 
   public void run() {
-    final long startTime = System.nanoTime();
-    endTimeInNs  = startTime + (runtime * 1000000000L);
+    startTimeInNs = System.nanoTime();
+    endTimeInNs  = startTimeInNs + (runtime * 1000000000L);
+    addShutdownHook();
     final DBOptions dbOptions = getDBOptions();
     final List<ColumnFamilyDescriptor> columnFamilyDescriptors = getColumnFamilies();
     final List<ColumnFamilyHandle> columnFamilyHandles = new ArrayList<>();
@@ -110,23 +134,33 @@ public class RocksDbWriteBenchmark implements Runnable {
         handlers.put(new String(handle.getName()), handle);
       }
 
+      writeOptions = new WriteOptions();
+      batch = new WriteBatch();
+
       final ExecutorService writeExecutor = Executors.newFixedThreadPool(writerThreads);
       for (int i = 0; i < this.writerThreads; i++) {
         writeExecutor.submit(this::writeKeys);
       }
+
+      ScheduledExecutorService statsThread = Executors.newSingleThreadScheduledExecutor();
+      statsThread.scheduleAtFixedRate(this::printStats, 5, 5, TimeUnit.MINUTES);
+
       writeExecutor.shutdown();
       writeExecutor.awaitTermination(Integer.MAX_VALUE, TimeUnit.MINUTES);
+
+      statsThread.shutdown();
+      statsThread.awaitTermination(Integer.MAX_VALUE, TimeUnit.MINUTES);
     } catch (Exception ex) {
       throw new RuntimeException(ex);
     } finally {
-      System.out.println("Time (sec) : " + ((System.nanoTime() - startTime)/1000000000L));
-      System.out.println("Number of keys: " + keyCount.get());
-      System.out.println("STALL_MICROS : " + statistics.getTickerCount(STALL_MICROS));
-      System.out.println("COMPACT_READ_BYTES: " + statistics.getTickerCount(COMPACT_READ_BYTES));
-      System.out.println("COMPACT_WRITE_BYTES: " + statistics.getTickerCount(COMPACT_WRITE_BYTES));
-      System.out.println("statistics: " + statistics.toString());
       if (db != null) {
-        db.close();
+        try {
+          db.write(writeOptions, batch);
+          batch.close();
+          db.close();
+        } catch (Exception ex) {
+          System.err.println("Exception while closing db: " + ex.getMessage());
+        }
       }
     }
   }
@@ -134,7 +168,6 @@ public class RocksDbWriteBenchmark implements Runnable {
   private DBOptions getDBOptions() {
     final int maxBackgroundCompactions = 4;
     final int maxBackgroundFlushes = 2;
-    final long bytesPerSync = toLong(StorageUnit.MB.toBytes(1.00));
     final boolean createIfMissing = true;
     final boolean createMissingColumnFamilies = true;
     final DBOptions options = new DBOptions();
@@ -143,7 +176,6 @@ public class RocksDbWriteBenchmark implements Runnable {
         .setIncreaseParallelism(Runtime.getRuntime().availableProcessors())
         .setMaxBackgroundCompactions(maxBackgroundCompactions)
         .setMaxBackgroundFlushes(maxBackgroundFlushes)
-        .setBytesPerSync(bytesPerSync)
         .setCreateIfMissing(createIfMissing)
         .setCreateMissingColumnFamilies(createMissingColumnFamilies);
     return options;
@@ -152,7 +184,6 @@ public class RocksDbWriteBenchmark implements Runnable {
   private List<ColumnFamilyDescriptor> getColumnFamilies() {
     final List<ColumnFamilyDescriptor> columnFamilies = new ArrayList<>();
     columnFamilies.add(new ColumnFamilyDescriptor(RocksDB.DEFAULT_COLUMN_FAMILY, getColumnFamilyOptions()));
-    columnFamilies.add(new ColumnFamilyDescriptor(OPEN_KEY_TABLE.getBytes(StandardCharsets.UTF_8), getColumnFamilyOptions()));
     columnFamilies.add(new ColumnFamilyDescriptor(KEY_TABLE.getBytes(StandardCharsets.UTF_8), getColumnFamilyOptions()));
     return columnFamilies;
   }
@@ -200,29 +231,20 @@ public class RocksDbWriteBenchmark implements Runnable {
           continue;
         }
 
-        // Write it to OpenKeyTable
-        //addToOpenKeyTable(key, buff.array());
-        addToOpenKeyMap(keyString, buff.array());
+        byte[] data = buff.array();
 
-        // Add block
+        // Add blocks
         for (int i = 1; i < numBlocks; i++) {
-          //final byte[] data = db.get(handlers.get(OPEN_KEY_TABLE), key);
-          final byte[] data = openKeys.get(keyString);
-          final byte[] block = new byte[100];
+          final byte[] block = new byte[500];
           ran.nextBytes(block);
           final ByteBuffer newBuff = ByteBuffer.wrap(new byte[data.length + block.length]);
           newBuff.put(data)
               .put(block);
-          //addToOpenKeyTable(key, newBuff.array());
-          addToOpenKeyMap(keyString, newBuff.array());
+          data = newBuff.array();
         }
 
-        // Move from OpenKeyTable to KeyTable
-        //final byte[] data = db.get(handlers.get(OPEN_KEY_TABLE), key);
-        db.put(handlers.get(KEY_TABLE), key, openKeys.get(keyString));
-        //db.delete(handlers.get(OPEN_KEY_TABLE), key);
-        openKeys.remove(keyString);
-
+        putToRocksDbBatch(KEY_TABLE, key, data);
+        commitToRocksDbIfRequired();
         keyCount.incrementAndGet();
       }
     } catch (Exception ex) {
@@ -230,18 +252,67 @@ public class RocksDbWriteBenchmark implements Runnable {
     }
   }
 
-  private void addToOpenKeyTable(byte[] key, byte[] value) throws RocksDBException {
-    db.put(handlers.get(OPEN_KEY_TABLE), key, value);
-  }
-
-  private void addToOpenKeyMap(String key, byte[] value) {
-    openKeys.put(key, value);
-  }
-
-
   private static long toLong(double value) {
     BigDecimal temp = BigDecimal.valueOf(value);
     return temp.longValue();
+  }
+
+  private void putToRocksDbBatch(String table, byte[] key, byte[] value) throws RocksDBException {
+    synchronized (this) {
+      batch.put(handlers.get(table), key, value);
+      currentBatchSize.incrementAndGet();
+    }
+  }
+
+  private void commitToRocksDbIfRequired() throws RocksDBException {
+    if (rocksDbBatchSize <= currentBatchSize.get()) {
+      synchronized (this) {
+        if (rocksDbBatchSize <= currentBatchSize.get()) {
+          final long start = System.nanoTime();
+          db.write(writeOptions, batch);
+          if (flush) {
+            db.flushWal(sync);
+            deltaFlushTime = deltaFlushTime + (System.nanoTime() - start);
+            deltaFlushCount++;
+            if (deltaFlushCount >= 100) {
+              System.err.println(deltaFlushTime / deltaFlushCount);
+              deltaFlushCount = 0;
+              deltaFlushTime = 0;
+            }
+          }
+          batch.close();
+          batch = new WriteBatch();
+          currentBatchSize.set(0);
+        }
+      }
+    }
+  }
+
+  private void addShutdownHook() {
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      try {
+        System.out.println("Final Stats!");
+        printStats();
+      } catch (Exception e) {
+        System.err.println("Encountered Exception while benchmarking OzoneManager!");
+        e.printStackTrace();
+      }
+    }));
+  }
+
+  private void printStats() {
+    System.out.println("================================================================");
+    System.out.println(LocalDateTime.now());
+    System.out.println("================================================================");
+    System.out.println("Time elapsed: " + ((System.nanoTime() - startTimeInNs)/1000000000)  + " sec.");
+    System.out.println("Number of keys: " + keyCount.get());
+    System.out.println("RocksDB Flush: " + flush);
+    System.out.println("RocksDB Sync: " + sync);
+    System.out.println("STALL_MICROS: " + statistics.getTickerCount(STALL_MICROS));
+    System.out.println("COMPACT_READ_BYTES: " + statistics.getTickerCount(COMPACT_READ_BYTES));
+    System.out.println("COMPACT_WRITE_BYTES: " + statistics.getTickerCount(COMPACT_WRITE_BYTES));
+    System.out.println("================================================================");
+
   }
 
 }
